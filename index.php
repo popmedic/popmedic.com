24cb78dd591baedce3e396e200831d174e44fe3e
popmedic <popmedic@gmail.com>
2013-03-08 11:47:59 -0700

    Added index and whats needed...

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=240,user-scalable=no" />
+<title>The PoPMediC</title>
+<style>
+body{
+	font-family:Helvetica;
+	font-size:12pt;
+	min-width:780px;
+	max-width:780px;
+	color:rgba(0, 0, 0, 0.5);
+}
+h1{
+	font-size:32pt;
+}
+h2{
+	font-size:24pt;
+}
+h3{
+	font-size:16pt;
+}
+a{
+	font-weight:bold;
+	border:none;
+	text-decoration:none;
+	color:rgba(0, 0, 0, 0.5);
+}
+a:hover{
+	color:rgba(0,0,0,1);
+}
+.center{
+	text-align:center;
+}
+.tiny-text{
+	font-size:8pt;
+}
+.dotted-box{
+	border-style:dotted;
+	border-width:1px;
+	padding:5px;
+}
+.gradient-bg {
+   	background-image: -ms-linear-gradient(bottom, #FFFFFF 0%, #000000 100%);
+	background-image: -moz-linear-gradient(bottom, #FFFFFF 0%, #000000 100%);
+	background-image: -o-linear-gradient(bottom, #FFFFFF 0%, #000000 100%);
+	background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, #FFFFFF), color-stop(1, #000000));
+	background-image: -webkit-linear-gradient(bottom, #FFFFFF 0%, #000000 100%);
+	background-image: linear-gradient(to top, #FFFFFF 0%, #000000 100%);
+}
+.banner{
+	width:100%;
+}
+.banner table{
+	padding:5px;
+	width:100%;
+	color:rgb(255, 255, 255);
+	background-color:rgba(0, 0, 0, 0.5);
+}
+.banner img{
+	width:128px;
+	height:128px;
+}
+.banner .txt{
+	padding-left:64px;
+}
+.deadpool-background{
+	background-image:url(images/deadpool-strip1.jpg);
+	background-size:100% 100%;
+	background-repeat:no-repeat;
+	width:720px;
+	height:340px;
+	margin:30px;
+	padding-left:4px;
+	padding-top:4px;
+	background-color:rgba(255, 255, 255, 0.95);
+	color:rgba(0, 0, 0, 0.80);
+}
+.deadpool-background .caption{
+	position:relative;
+	width:128px;
+	background-color:rgba(255, 255, 255, 0.60);
+	padding-bottom:4px;
+}
+.deadpool-background .h3{
+	font-size:16pt;
+	font-weight:bold;
+}
+.deadpool-background a{
+	font-size:12pt;
+	font-weight:normal;
+	color:rgba(0, 0, 0, 0.90);
+	background-color:inherit;
+}
+.deadpool-background a:hover{
+	color:rgba(255,255,255,0.75);
+	background-color:inherit;
+}
+
+.monks-background{
+	background-image:url(images/monks-and-penny.png);
+	background-size:720px;
+	background-repeat:no-repeat;
+	height:720px;
+	padding-left:410px;
+	background-color:rgba(255, 255, 255, 0.75);
+	color:rgba(64, 64, 64, 0.75);
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+}
+.monks-background .h3{
+	font-size:16pt;
+	font-weight:bold;
+	background-color:rgba(255, 255, 255, 0.75);
+	color:rgba(64, 64, 64, 0.75);
+}
+.monks-background a{
+	background-color:rgba(255, 255, 255, 0.75);
+	color:rgba(64, 64, 64, 0.75);
+	font-size:12pt;
+	font-weight:normal;
+}
+.monks-background a:hover{
+	color:rgba(0,0,0,0.25);
+}
+</style>
+</head>
+
+<body>
+	<div class="banner">
+		<table class="gradient-bg">
+			<tr>
+				<td width="138px">
+					<img src="<?php print "http://www.gravatar.com/avatar/" . md5("popmedic@gmail.com") . "?d=" . urlencode("http://popmedic.com") . "&s=128"; ?>" />
+				</td>
+				<td>
+					<h1 class="txt">t h e&nbsp;&nbsp;&nbsp;&nbsp;P&nbsp;&nbsp;O&nbsp;&nbsp;P&nbsp;&nbsp;M&nbsp;&nbsp;E&nbsp;&nbsp;D&nbsp;&nbsp;I&nbsp;&nbsp;C</h1>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div class="center">
+		<!--hr / -->
+		<!--h2 class="center">
+			You still carrying him on your back???
+		</h2-->
+		<div class="deadpool-background" style="text-align:left;">
+        	<div class="caption">
+			<span class="h3">
+				About:
+			</span>
+			<ul>
+				<li>
+					<a href="http://www.facebook.com/popmedic" target="_blank">Facebook</a>
+				</li>
+				<li>
+					<a href="http://www.linkedin.com/profile/view?id=41362576&trk=tab_pro" target="_blank">Linkedin</a>
+				</li>
+				<li>
+					<a href="https://github.com/popmedic" target="_blank">Github</a>
+				</li>
+				<li>
+					<a href="http://download.cnet.com/mac/popmedic/3260-20_4-10223948.html?tag=rb_content;contentBody" target="_blank">Cnet</a>
+				</li>
+			</ul>
+            </div>
+			<!--span class="h3">
+				Projects:
+			</span>
+			<ul>
+				<li>
+					<a href="https://github.com/popmedic/mp4autotag#mp4autotag" target="_blank">Mp4Autotag</a>
+				</li>
+				<li>
+					<a href="http://popmedic.com/cgi/mp4autotag_cgi.rb" target="_blank">Mp4Autotag Server CGI</a>
+				</li>
+				<li>
+					<a href="http://popmedic.com/popmsplit" target="_blank">PopMSplit</a>
+				</li>
+			</ul>
+			<span class="h3">
+				Sites:
+			</span>
+			<ul>
+				<li>
+					<a href="http://www.shanysmpg.com" target="_blank">Shany's Mobile Pet Grooming</a>
+				</li>
+				<li>
+					<a href="http://www.straightlinepainting.us" target="_blank">Straight Line Painting</a>
+				</li>
+			</ul-->
+        </div>
+		<!--h2>
+			Dang it penny! Get outta there!
+		</h2-->
+		<p>
+			<span class="center tiny-text dotted-box">
+				created by <a href="http://popmedic.com" alt="Infinite Loop.">The Popmedic</a>
+			</span>
+		</p>
+	</div>
+</body>
+</html>
